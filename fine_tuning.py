d6a6c17b36b7fbab3b4ea58daef6a20e6213ca84
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-11-18 00:02:57 +0900

    エラーハンドリング追加

--- fine_tuning.py
+++ fine_tuning.py
@@ -4,5 +4,17 @@ import os
 
 load_dotenv()
 
-openai.api_key = os.getenv('OPENAI_API_KEY')
-openai.FineTuningJob.create(training_file="test_data.jsonl", model="gpt-3.5-turbo")
+api_key = os.getenv('OPENAI_API_KEY')
+if api_key is None:
+    raise ValueError("The OPENAI_API_KEY environment variable is not set.")
+
+openai.api_key = api_key
+
+try:
+    if not os.path.isfile("test_data.jsonl"):
+        raise FileNotFoundError("The training file test_data.jsonl was not found.")
+
+    openai.FineTuningJob.create(training_file="test_data.jsonl", model="gpt-3.5-turbo")
+    print("Fine-tuning job created successfully.")
+except Exception as e:
+    print(f"An error occurred while creating the fine-tuning job: {e}")
