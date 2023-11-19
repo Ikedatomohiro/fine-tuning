a48d9a5d34d6bb4d2a2c98c0d86724b043f972f0
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-11-19 15:58:26 +0900

    typo修正

--- gpt-test/gpt-test.py
+++ gpt-test/gpt-test.py
@@ -17,7 +17,7 @@ client = OpenAI(
 response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "Your aer a helpful assistant."},
+        {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "Hello! I'm Tom"}
     ]
 )
