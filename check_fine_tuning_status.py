f471147cd663ca29e910bb7a220b967abde09708
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-11-23 21:08:39 +0900

    main関数を作成

--- check_fine_tuning_status.py
+++ check_fine_tuning_status.py
@@ -2,12 +2,16 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def main():
+  load_dotenv()
 
-client = OpenAI(
-  api_key=os.getenv('OPENAI_API_KEY'),
-)
+  client = OpenAI(
+    api_key=os.getenv('OPENAI_API_KEY'),
+  )
 
-response = client.fine_tuning.jobs.list()
+  response = client.fine_tuning.jobs.list()
 
-print(response)
+  print(response)
+
+if __name__ == '__main__':
+  main()
