49bf4b42bf62691ffbad7d575ba96a77d52e63b3
Tomohiro Ikeda <to.tomo.tomohiro@gmail.com>
2023-11-23 21:01:53 +0900

    ifからmainを呼ぶように変更

--- gpt-test/gpt-test.py
+++ gpt-test/gpt-test.py
@@ -2,18 +2,22 @@ from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+def main():
+  load_dotenv()
 
-client = OpenAI(
+  client = OpenAI(
   api_key=os.getenv('OPENAI_API_KEY'),
-)
+  )
 
-response = client.chat.completions.create(
+  response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "あなたは、Go言語に詳しいエンジニアです。Goに関する質問に回答してください。"},
         {"role": "user", "content": "Go言語の特徴はなんですか？"}
     ]
-)
+  )
 
-print(response)
+  print(response)
+
+if __name__ == '__main__':
+  main()
